42e6ca4afba45ccc89b9d2106cb227de76e7114f
karimtounsi <karim.tounsi100@gmail.com>
2021-01-30 11:27:00 +0100

    EmployeeRepository

---
+++ src/main/java/com/ktounsi/assessment/employee/repository/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.ktounsi.assessment.employee.repository;
+
+
+import com.ktounsi.assessment.employee.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+
+
+}

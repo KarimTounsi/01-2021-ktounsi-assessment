2b29721774d75ee0f01809e828e5f36a6c2af52f
karimtounsi <karim.tounsi100@gmail.com>
2021-01-30 11:20:52 +0100

    AddressRepository

---
+++ src/main/java/com/ktounsi/assessment/address/repository/AddressRepository.java
@@ -0,0 +1,12 @@
+package com.ktounsi.assessment.address.repository;
+
+import com.ktounsi.assessment.address.entity.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface AddressRepository extends JpaRepository<Address, Long> {
+
+
+
+}
